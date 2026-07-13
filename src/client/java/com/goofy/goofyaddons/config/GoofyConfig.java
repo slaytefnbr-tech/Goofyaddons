d1a3220f3dc7d5288d36e5a1ca8641fde23b097f
slaytefnbr-tech <slaytefnbr@gmail.com>
2026-07-13 20:12:45 +0100

    Update GoofyConfig.java

--- src/client/java/com/goofy/goofyaddons/config/GoofyConfig.java
+++ src/client/java/com/goofy/goofyaddons/config/GoofyConfig.java
@@ -18,8 +18,8 @@ public class GoofyConfig {
     public GoofyConfig() {
         books.add(new Book("ENCHANTMENT_ULTIMATE_WISE", 1, 5, "Ultimate Wise"));
         books.add(new Book("ENCHANTMENT_ULTIMATE_WISE", 2, 5, "Ultimate Wise"));
-        // books.add(new Book("ENCHANTMENT_ULTIMATE_WISDOM", 1, 5, "Wisdom"));
-        // books.add(new Book("ENCHANTMENT_ULTIMATE_WISDOM", 2, 5, "Wisdom"));
+         books.add(new Book("ENCHANTMENT_ULTIMATE_WISDOM", 1, 5, "Wisdom"));
+         books.add(new Book("ENCHANTMENT_ULTIMATE_WISDOM", 2, 5, "Wisdom"));
         // books.add(new Book("ENCHANTMENT_ULTIMATE_LAST_STAND", 1, 5, "Last Stand"));
         // books.add(new Book("ENCHANTMENT_ULTIMATE_LAST_STAND", 2, 5, "Last Stand"));
     }
